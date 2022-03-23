6461c78b89118901f21efb177e95d4b18c2206eb
Connor <cimok2000@gmail.com>
2022-03-23 09:43:49 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("Weather CLI")
+        .version("0.1.0")
+        .author("Connor O'Keefe <cimok2000@gmail.com>")
+        .about("Pulls weather data from <replace with API name>")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .takes_value(true)
+                .help("An input file."),
+        )
+        .arg(
+            Arg::new("num")
+                .short('n')
+                .long("number")
+                .takes_value(true)
+                .help("A number"),
+        )
+        .get_matches();
+
+    let input_file = matches.value_of("file").unwrap_or("input.txt");
+    println!("The file passed is: {}", input_file);
+}
