b0b0de8cb25b36343dfcd2b98b9b964df9f334dc
Connor <cimok2000@gmail.com>
2022-03-23 09:45:38 +0000

    switched to support new Command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use clap::{App, Arg};
+use clap::{Arg, Command};
 
 fn main() {
-    let matches = App::new("Weather CLI")
+    let matches = Command::new("Weather CLI")
         .version("0.1.0")
         .author("Connor O'Keefe <cimok2000@gmail.com>")
         .about("Pulls weather data from <replace with API name>")
